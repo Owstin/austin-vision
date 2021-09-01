d1f02b794ac2b5256b1b472bc20941c23de81f6c
Austin Moore <owstingreen@gmail.com>
2021-09-01 07:52:51 -0500

    change output directory to docs

--- docs/src.e31bb0bc.js
+++ docs/src.e31bb0bc.js
@@ -255,7 +255,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62259" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53846" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
