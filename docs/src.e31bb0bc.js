9cafa5f06d53c24f792e884d5806a4f2e0827418
Austin Moore <owstingreen@gmail.com>
2021-09-01 08:06:20 -0500

    change public url

--- docs/src.e31bb0bc.js
+++ docs/src.e31bb0bc.js
@@ -255,7 +255,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53846" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60649" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -432,4 +432,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=/src.e31bb0bc.js.map
+//# sourceMappingURL=src.e31bb0bc.js.map
